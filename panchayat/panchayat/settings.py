973c5f0e2a2fcbb2cf5f231f0fdac87f0e438b02
sanjaysupanch <sanjay.k17@iiits.in>
2019-12-08 13:42:16 +0530

    4th  and 5th services completed

--- panchayat/panchayat/settings.py
+++ panchayat/panchayat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=o17(mz-k*55ou1!5@psy*-fof5nc8_=+kcj5+f&8km7%t!&zw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
